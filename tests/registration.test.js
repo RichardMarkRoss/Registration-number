13673265c24dcabf78338ab0ae96b0504e1c600a
RichardMarkRoss <cptrichkidprice@gmail.com>
2018-05-31 10:17:16 +0200

    commit

--- tests/registration.test.js
+++ tests/registration.test.js
@@ -55,9 +55,9 @@ describe('the registration number  filter test', function () {
 
         var nameHolder = RegNumber();
 
-        nameHolder.store("CA 123");
-        nameHolder.store("CA 123");
-        nameHolder.store('CZ 123');
+        nameHolder.store("CA 123");//True
+        nameHolder.store("CA 123");//False
+        nameHolder.store('CZ 123');//False
 
         assert.deepEqual(nameHolder.return(), {
             'CA 123': 0
@@ -67,12 +67,12 @@ describe('the registration number  filter test', function () {
 
         var nameHolder = RegNumber();
 
-        nameHolder.store("CA 123");
-        nameHolder.store("CY 123");
-        nameHolder.store('CZ 123');
-        nameHolder.store('CK 123');
-        nameHolder.store('CH 123');
-        nameHolder.store('CJ 123');
+        nameHolder.store("CA 123");//True
+        nameHolder.store("CY 123");//True
+        nameHolder.store('CZ 123');//False
+        nameHolder.store('CK 123');//False
+        nameHolder.store('CH 123');//False
+        nameHolder.store('CJ 123');//True
 
         assert.deepEqual(nameHolder.return(), {
             'CA 123': 0,
